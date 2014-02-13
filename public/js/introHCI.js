1e5ee097970396841b4c866572cf15217bee1be1
Yury Ogienko <yuryogienko@gmail.com>
2014-02-13 12:38:23 -0800

    putting it together lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,10 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	console.log('/project/' + idNumber);
+
+	$.get('/project/' + idNumber, addProject);
 }
 
 /*
@@ -35,4 +39,25 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', colorIt);
+}
+
+function addProject(result) {
+	//console.log(result);
+	var projectHTML = '<div class="thumbnail"><a href="#">' +
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<h3>' + result['title'] + '</h3>' +
+    '<p><small>' + result['date'] + '</small></p></a>' + 
+    result['summary'] + '</div>';
+
+    $("#project" + result.id + " .details").html(projectHTML);
+}
+
+function colorIt(colors) {
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
